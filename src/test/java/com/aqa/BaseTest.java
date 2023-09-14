21d71402392c5ad2d29717aa638957d52101b116
aqaacc <mralextb@gmail.com>
2023-09-14 14:25:28 +0200

    added 3 tests for Login feature

---
+++ src/test/java/com/aqa/BaseTest.java
@@ -0,0 +1,33 @@
+package com.aqa;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        //initialize web driver and create driver instance
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        //Set up browser and waiting
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
